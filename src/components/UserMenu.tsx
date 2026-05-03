b5388ab83ebcb663bb2f29729e013a9f5f2c3399
Hendra Sadewa <me@hendrasadewa.com>
2026-05-03 16:34:52 +0700

    fix: remove unused imports

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -1,11 +1,7 @@
 import { UserIcon } from 'lucide-react'
 import { Avatar, AvatarFallback } from "./ui/avatar";
-import { useAuthContext } from "@/hooks/use-auth-context";
 
 export function UserMenu() {
-  const { user } = useAuthContext();
-  
-  const initial = user?.displayName.at(0) ?? "U";
 
   return (
     <div className="flex items-center gap-2">
